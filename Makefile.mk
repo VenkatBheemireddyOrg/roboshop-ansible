b4f441b7ec9068ab12a326692cb99b531bee90f0
venkat <devops.bheemireddy@gmail.com>
2024-12-25 22:35:30 +1100

    learn-ansible changes

--- Makefile.mk
+++ Makefile.mk
@@ -1,5 +1,4 @@
-default:
-   ansible-playbook -i $(app_name)-dev.azdevopsv82.online, -e ansible_user=venkat -e ansible_password=Devops123456 -e app_name=$(app_name) roboshop.yml
-
-ansible-playbook -i $(app_name)-dev.azdevopsb82.online, -e ansible_user=raghu -e ansible_password=DevOps123456 -e ENV=dev -e app_name=$(app_name) roboshop.yml
+dev:
+  git pull
+  ansible-playbook -i $(app_name)-dev.azdevopsb82.online, -e ansible_user=raghu -e ansible_password=DevOps123456 -e ENV=dev -e app_name=$(app_name) roboshop.yml
 
