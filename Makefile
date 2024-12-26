a50b088e601d136a21c7c71e5175090582e595dc
venkat <devops.bheemireddy@gmail.com>
2024-12-26 23:08:54 +1100

    roboshop-ansible project changes

---
+++ Makefile
@@ -0,0 +1,3 @@
+default:
+ ansible-playbook -i $(app_name).azdevopsv82.online, -e ansible_user=venkat -e ansible_password=DevOps123456 -e app=$(app) roboshop.yml
+
--- Makefile.mk
+++
@@ -1,3 +0,0 @@
-default:
- ansible-playbook -i $(app_name)-dev.azdevopsb82.online, -e ansible_user=raghu -e ansible_password=DevOps123456 -e ENV=dev -e app_name=$(app_name) roboshop.yml
-
